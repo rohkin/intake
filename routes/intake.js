9f66c590180ca7e55b47be814c0e86d2dedc100a
Rohkin <sir.james.what@gmail.com>
2015-12-28 10:58:16 +0100

    intake views

--- routes/intake.js
+++ routes/intake.js
@@ -3,8 +3,7 @@ module.exports = function (app) {
                 .get(function (request, response) {
                         var options = [{id:0,value:"test0",selected:false},{id:1,value:"test1",selected:true}];
                         var buttons = [{href:"intake/list",class:"btn-default",value:"Annuleren"},
-                                        {class:"btn-success",value:"Toevoegen"},
-                                        {class:"btn-danger", value:"Verwijderen"}];
+                                        {class:"btn-success",value:"Toevoegen"}];
                         response.render("intake/add", {options:options,buttons:buttons});
                 });
         app.route('/intake/delete')
@@ -17,7 +16,8 @@ module.exports = function (app) {
                 });
         app.route('/intake/list')
                 .get(function (request, response) {
-                        response.render("intake/list");
+                        var list_items = [{id:0,value:"test1"},{id:1,value:"test2"},{id:2,value:"test3"}];
+                        response.render("intake/list",{list_items:list_items});
                 });
         app.route('/intake/show')
                 .get(function (request, response) {
